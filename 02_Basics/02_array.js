b7f10bf187214de365014d7e48b12f74206b7941
Ritik12062002 <153264986+Ritik12062002@users.noreply.github.com>
2024-07-18 16:53:27 +0000

    arrays in js

---
+++ 02_Basics/02_array.js
@@ -0,0 +1,26 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman",]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][0]);
+
+//  const newArr=marvel_heros.concat(dc_heros)
+// console.log(newArr);
+
+// spread operator
+// const allNew =[ ...marvel_heros, ...dc_heros]
+// console.log(allNew);
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+// console.log(Array.isArray("ritik"));
+// console.log(Array.from("ritik"));
+
+console.log(Array.from({name:"ritik"}));
+let s1=100
+let s2=200
+let s3=300
+console.log(Array.of(s1,s2,s3));

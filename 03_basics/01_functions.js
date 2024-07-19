adc55b721e0f421d03793879d3bb5469197920db
Ritik12062002 <153264986+Ritik12062002@users.noreply.github.com>
2024-07-19 18:33:10 +0000

    functions in js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,4 +28,41 @@ function loginUserMessage(username= "rit"){
         return `${username} just logged in.`
 }
 }
-console.log(loginUserMessage("ritik"));
+// console.log(loginUserMessage("ritik"));
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+function calculatorCartPrice(val1, val2,...n1){
+    return n1
+}
+// console.log(calculatorCartPrice(200,300,405,2000));
+
+// function with object
+
+
+const user ={
+    username:"ritik",
+    price:200
+}
+
+function handleObject(anyObject){
+    return `Username is ${anyObject.username} and price is ${anyObject.price}`
+}
+
+// console.log(handleObject(user));
+console.log(handleObject({
+    username:"sik",
+    price:399
+}));
+
+const myNewArr =[100,200,300]
+
+
+//function with array
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArr));
+console.log(returnSecondValue([100,200,300,4000]));

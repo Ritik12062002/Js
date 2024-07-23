e1ff715be8085a6c59c820e2fb0a082d5bc6e11c
Ritik12062002 <153264986+Ritik12062002@users.noreply.github.com>
2024-07-23 18:32:52 +0000

    While and do while loop in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,34 @@
+//while
+// syntax
+// while (condition) {
+    
+// }
+
+
+// let i=0;
+
+// while (i<=10) {
+//     console.log(`Value of i is ${i}`);
+//     i=i+2
+// }
+
+// let myArray =['a','b','s']
+// let arr=0
+// while(arr<myArray.length){
+//     console.log(`value is ${myArray[arr]}`);
+//     arr++
+// }
+
+
+//do while
+//syntax
+// do {
+    
+// } while (condition);
+
+
+let score=11;
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score<=10)
